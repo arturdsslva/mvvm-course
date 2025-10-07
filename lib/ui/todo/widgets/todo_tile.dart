dea065e21ed6baea3777f4352110812eff25684a
arturdsslva <arturdsslva@gmail.com>
2025-10-06 21:05:19 -0300

    refactor todo tile

--- lib/ui/todo/widgets/todo_tile.dart
+++ lib/ui/todo/widgets/todo_tile.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/typedefs/todo.types.dart';
 import '../../../domain/models/todo.model.dart';
 
 class TodoTile extends StatelessWidget {
+  final DeleteTodo delete;
   final Todo todo;
-  const TodoTile({super.key, required this.todo});
+  const TodoTile({super.key, required this.todo, required this.delete});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(leading: Text(todo.id.toString()), title: Text(todo.title));
+    return ListTile(
+      leading: Text(todo.id.toString()),
+      title: Text(todo.title),
+      trailing: IconButton(
+        onPressed: () => delete(todo),
+        icon: const Icon(Icons.delete, color: Colors.red),
+      ),
+    );
   }
 }
