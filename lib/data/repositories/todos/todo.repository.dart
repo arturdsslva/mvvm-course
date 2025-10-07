913632db8a3ea96c1196c6746ffd672442337154
arturdsslva <arturdsslva@gmail.com>
2025-10-06 21:04:05 -0300

    create todo repo interface

---
+++ lib/data/repositories/todos/todo.repository.dart
@@ -0,0 +1,8 @@
+import '../../../core/result/result.dart';
+import '../../../domain/models/todo.model.dart';
+
+abstract class TodoRepository {
+  Future<Result<List<Todo>>> get();
+  Future<Result<Todo>> create(String title);
+  Future<Result<void>> delete(Todo todo);
+}
