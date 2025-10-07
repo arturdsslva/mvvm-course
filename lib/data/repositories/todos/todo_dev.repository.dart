c53a98de059e0d8bf1177ab136c772a224e8eaeb
arturdsslva <arturdsslva@gmail.com>
2025-10-06 21:04:22 -0300

    create todo dev repo implementation

---
+++ lib/data/repositories/todos/todo_dev.repository.dart
@@ -0,0 +1,31 @@
+import 'package:mvvm_course/core/result/result.dart';
+
+import 'package:mvvm_course/domain/models/todo.model.dart';
+
+import 'todo.repository.dart';
+
+class TodoDevRepository implements TodoRepository {
+  final List<Todo> _todos = [];
+  @override
+  Future<Result<Todo>> create(String title) async {
+    final lastIndex = _todos.length;
+    final newTodo = Todo(id: lastIndex + 1, title: title);
+    _todos.add(newTodo);
+    return Result.ok(newTodo);
+  }
+
+  @override
+  Future<Result<void>> delete(Todo todo) {
+    if (_todos.contains(todo)) {
+      _todos.remove(todo);
+      return Future.value(Result.ok(null));
+    } else {
+      return Future.value(Result.error(Exception('Todo not found')));
+    }
+  }
+
+  @override
+  Future<Result<List<Todo>>> get() {
+    return Future.value(Result.ok(List.unmodifiable(_todos)));
+  }
+}
