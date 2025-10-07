7dd05dc08d902b7b9f2a4d1dfa1821db3e8602c5
arturdsslva <arturdsslva@gmail.com>
2025-10-06 21:05:03 -0300

    refator todo screen

--- lib/ui/todo/widgets/todo.screen.dart
+++ lib/ui/todo/widgets/todo.screen.dart
@@ -3,9 +3,30 @@ import 'package:mvvm_course/ui/todo/view_models/todo.view_model.dart';
 import 'package:mvvm_course/ui/todo/widgets/add_todo_dialog.dart';
 import 'package:mvvm_course/ui/todo/widgets/todo_list.dart';
 
-class TodoScreen extends StatelessWidget {
+import '../../../domain/models/todo.model.dart';
+
+class TodoScreen extends StatefulWidget {
   final TodoViewModel viewModel;
   const TodoScreen({super.key, required this.viewModel});
+
+  @override
+  State<TodoScreen> createState() => _TodoScreenState();
+}
+
+class _TodoScreenState extends State<TodoScreen> {
+  TodoViewModel get viewModel => widget.viewModel;
+
+  @override
+  void initState() {
+    viewModel.delete.addListener(_onDelete);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    viewModel.delete.removeListener(_onDelete);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +46,7 @@ class TodoScreen extends StatelessWidget {
         child: ListenableBuilder(
           listenable: viewModel,
           builder: (BuildContext context, Widget? child) {
-            return TodoList(todos: viewModel.todos);
+            return TodoList(todos: viewModel.todos, delete: delete);
           },
         ),
       ),
@@ -41,5 +62,35 @@ class TodoScreen extends StatelessWidget {
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  void delete(Todo todo) {
+    viewModel.delete.execute(todo);
+  }
+
+  void _onDelete() {
+    if (viewModel.delete.running) {
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return IntrinsicHeight(
+            child: const Center(child: CircularProgressIndicator()),
+          );
+        },
+      );
+    } else {
+      Navigator.of(context).pop();
+      ScaffoldMessenger.of(context).removeCurrentSnackBar();
+      if (viewModel.delete.completed) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Todo deleted successfully')),
+        );
+      } else {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('Error: error deleting todo')));
+      }
+    }
   }
 }
