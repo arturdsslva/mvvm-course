e5072398341a9674ab4ce154efcd60ec3a766181
arturdsslva <arturdsslva@gmail.com>
2025-10-06 21:05:13 -0300

    refactor todo list

--- lib/ui/todo/widgets/todo_list.dart
+++ lib/ui/todo/widgets/todo_list.dart
@@ -1,12 +1,14 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:mvvm_course/domain/models/todo.model.dart';
-import 'package:mvvm_course/ui/todo/widgets/todo_tile.dart';
+
+import '../../../core/typedefs/todo.types.dart';
+import '../../../domain/models/todo.model.dart';
+import 'todo_tile.dart';
 
 class TodoList extends StatelessWidget {
+  //TODO: do like this, with callback
+  final DeleteTodo delete;
   final List<Todo> todos;
-  const TodoList({super.key, required this.todos});
+  const TodoList({super.key, required this.todos, required this.delete});
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +19,7 @@ class TodoList extends StatelessWidget {
       itemCount: todos.length,
       itemBuilder: (context, index) {
         final todo = todos[index];
-        return TodoTile(todo: todo);
+        return TodoTile(todo: todo, delete: delete);
       },
     );
   }
