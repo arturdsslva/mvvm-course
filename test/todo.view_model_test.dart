278ebca0f3cf67190faf937e62f3a3efa9b1ef8f
arturdsslva <arturdsslva@gmail.com>
2025-10-06 23:03:43 -0300

    fix tests

--- test/todo.view_model_test.dart
+++ test/todo.view_model_test.dart
@@ -1,16 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mvvm_course/data/repositories/todos/todo_dev.repository.dart';
 import 'package:mvvm_course/ui/todo/view_models/todo.view_model.dart';
 
 void main() {
   group('TodoViewModel -', () {
+    late TodoViewModel viewModel;
+    late TodoDevRepository repository;
+    setUp(() {
+      repository = TodoDevRepository();
+      viewModel = TodoViewModel(todoRepository: repository);
+    });
     test('verify initial state', () {
-      final TodoViewModel viewModel = TodoViewModel();
       expect(viewModel.todos, isEmpty);
       expect(viewModel.load, isNotNull);
       expect(viewModel.create, isNotNull);
     });
     test('should create todo', () async {
-      final TodoViewModel viewModel = TodoViewModel();
       expect(viewModel.todos, isEmpty);
 
       await viewModel.create.execute('New Todo');
@@ -20,7 +25,6 @@ void main() {
       expect(viewModel.todos.first.title, 'New Todo');
     });
     test('should delete todo', () async {
-      final TodoViewModel viewModel = TodoViewModel();
       expect(viewModel.todos, isEmpty);
 
       await viewModel.create.execute('New Todo');
