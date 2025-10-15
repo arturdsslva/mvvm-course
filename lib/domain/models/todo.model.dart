8a98a139c60b7c14b34b92273e257668f96a8266
arturdsslva <arturdsslva@gmail.com>
2025-10-14 23:20:14 -0300

    add equality n copyWith in Todo model class

--- lib/domain/models/todo.model.dart
+++ lib/domain/models/todo.model.dart
@@ -3,7 +3,21 @@ class Todo {
   final String title;
   Todo({this.id, required this.title});
 
-  factory Todo.fromJson(Map<String, dynamic> json) {
-    return Todo(id: json['id'] as String, title: json['title'] as String);
+  Todo copyWith({String? id, String? title}) {
+    return Todo(id: id ?? this.id, title: title ?? this.title);
   }
+
+  @override
+  String toString() {
+    return 'Todo{id: $id, title: $title}';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is Todo && other.id == id && other.title == title;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ title.hashCode;
 }
