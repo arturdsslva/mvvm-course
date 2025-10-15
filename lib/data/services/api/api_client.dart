ce72a2b255b3c75d2c3d2352777c80aabc9e1ee6
arturdsslva <arturdsslva@gmail.com>
2025-10-14 23:20:56 -0300

    adjust api client to model n mapper changes

--- lib/data/services/api/api_client.dart
+++ lib/data/services/api/api_client.dart
@@ -2,7 +2,7 @@ import 'dart:convert' show Utf8Decoder, jsonDecode, jsonEncode;
 import 'dart:io' show HttpClient, HttpException;
 
 import 'package:mvvm_course/utils/result/result.dart';
-
+import 'package:mvvm_course/extensions/todo.mapper.dart';
 import '../../../domain/models/todo.model.dart';
 
 class ApiClient {
@@ -24,7 +24,9 @@ class ApiClient {
       if (response.statusCode == 200) {
         final stringData = await response.transform(const Utf8Decoder()).join();
         final jsonData = jsonDecode(stringData) as List<dynamic>;
-        final List<Todo> todos = jsonData.map((e) => Todo.fromJson(e)).toList();
+        final List<Todo> todos = jsonData
+            .map((e) => (e as Map<String, dynamic>).toTodo())
+            .toList();
         return Result.ok(todos);
       }
 
@@ -36,17 +38,17 @@ class ApiClient {
     }
   }
 
-  Future<Result<Todo>> postTodo(String title) async {
+  Future<Result<Todo>> postTodo(Todo todo) async {
     final client = _clientHttpFactory();
     try {
       final request = await client.post(_host, _port, '/todos');
-      request.write(jsonEncode({'title': title}));
+      request.write(jsonEncode(todo.toCreateJson()));
       final response = await request.close();
 
       if (response.statusCode == 201) {
         final stringData = await response.transform(Utf8Decoder()).join();
         final jsonData = jsonDecode(stringData) as Map<String, dynamic>;
-        final Todo newTodo = Todo.fromJson(jsonData);
+        final Todo newTodo = jsonData.toTodo();
         return Result.ok(newTodo);
       }
 
