4452e6eed54bebde27c3917361658d7a8e9478c7
arturdsslva <arturdsslva@gmail.com>
2025-10-31 00:17:32 -0300

    add navigation

--- lib/ui/todo/widgets/todo_tile.dart
+++ lib/ui/todo/widgets/todo_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../routing/routes.dart';
 import '../../../utils/typedefs/todo.types.dart';
 import '../../../domain/models/todo.model.dart';
 
@@ -10,12 +12,17 @@ class TodoTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Text(todo.id.toString()),
-      title: Text(todo.title),
-      trailing: IconButton(
-        onPressed: () => delete(todo),
-        icon: const Icon(Icons.delete, color: Colors.red),
+    return GestureDetector(
+      onTap: () => context.push(Routes.todoDetail(todo.id!)),
+      child: Card(
+        child: ListTile(
+          leading: Text(todo.id.toString()),
+          title: Text(todo.title),
+          trailing: IconButton(
+            onPressed: () => delete(todo),
+            icon: const Icon(Icons.delete, color: Colors.red),
+          ),
+        ),
       ),
     );
   }
