2312403e8bb297a87a852aa89e2c319ece050e57
arturdsslva <arturdsslva@gmail.com>
2025-11-16 23:32:32 -0300

    add vm on details and change repo usage

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -6,29 +6,32 @@ import '../data/repositories/todos/todos_remote.repository.dart';
 import '../data/services/api/api_client.dart';
 import '../ui/todo/view_models/todo.view_model.dart';
 import '../ui/todo/widgets/todo.screen.dart';
+import '../ui/todo_details/view_models/todo_details.view_model.dart';
 
 GoRouter routerConfig() {
+  final todoRepo = TodosRemoteRepository(
+    apiClient: ApiClient(host: "192.168.2.104"),
+  );
+
   return GoRouter(
     initialLocation: Routes.todos,
     routes: [
       GoRoute(
         path: Routes.todos,
         builder: (context, state) {
-          return TodoScreen(
-            viewModel: TodoViewModel(
-              todoRepository: TodosRemoteRepository(
-                apiClient: ApiClient(host: "192.168.2.104"),
-              ),
-            ),
-          );
+          return TodoScreen(viewModel: TodoViewModel(todoRepository: todoRepo));
         },
         routes: [
           GoRoute(
             path: ':id',
             builder: (context, state) {
               final id = state.pathParameters['id']!;
+              final TodoDetailsViewModel todoDetailsViewModel =
+                  TodoDetailsViewModel(todoRepository: todoRepo);
 
-              return TodoDetailsScreen(id: id);
+              todoDetailsViewModel.load.execute(id);
+
+              return TodoDetailsScreen(viewModel: todoDetailsViewModel, id: id);
             },
           ),
         ],
