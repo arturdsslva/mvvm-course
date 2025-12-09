527720dc37fc6cb8b50ea909146fbbc696d285c9
arturdsslva <arturdsslva@gmail.com>
2025-12-09 00:21:43 -0300

    add missing values

--- lib/domain/models/todo.model.dart
+++ lib/domain/models/todo.model.dart
@@ -1,27 +1,37 @@
 class Todo {
-  final String? id;
+  final String id;
   final String title;
-  Todo({this.id, required this.title});
+  final String desc;
+  final bool done;
+  Todo({
+    required this.id,
+    required this.title,
+    required this.desc,
+    required this.done,
+  });
 
-  factory Todo.fromJson(Map<String, dynamic> json) {
-    return Todo(id: json['id'] as String?, title: json['title'] as String);
-  }
-
-  Todo copyWith({String? id, String? title}) {
-    return Todo(id: id ?? this.id, title: title ?? this.title);
-  }
+  Todo.fromJson(Map<String, dynamic> json)
+    : id = json['id'] as String,
+      title = json['title'] as String,
+      desc = json['desc'] as String,
+      done = json['done'] as bool;
 
   @override
   String toString() {
-    return 'Todo{id: $id, title: $title}';
+    return 'Todo{id: $id, title: $title, desc: $desc, done: $done}';
   }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-    return other is Todo && other.id == id && other.title == title;
+    return other is Todo &&
+        other.id == id &&
+        other.title == title &&
+        other.desc == desc &&
+        other.done == done;
   }
 
   @override
-  int get hashCode => id.hashCode ^ title.hashCode;
+  int get hashCode =>
+      id.hashCode ^ title.hashCode ^ desc.hashCode ^ done.hashCode;
 }
