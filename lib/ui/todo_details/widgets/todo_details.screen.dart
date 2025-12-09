1a2a216972d34740ab7931222fa028c01c5f3e09
arturdsslva <arturdsslva@gmail.com>
2025-12-09 00:24:45 -0300

    add title in details

--- lib/ui/todo_details/widgets/todo_details.screen.dart
+++ lib/ui/todo_details/widgets/todo_details.screen.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_course/ui/todo_details/widgets/todo_name.widget.dart';
 import '../view_models/todo_details.view_model.dart';
 
 class TodoDetailsScreen extends StatefulWidget {
   final TodoDetailsViewModel viewModel;
-  final String id;
-  const TodoDetailsScreen({
-    super.key,
-    required this.viewModel,
-    required this.id,
-  });
+  const TodoDetailsScreen({super.key, required this.viewModel});
 
   @override
   State<TodoDetailsScreen> createState() => _TodoDetailsScreenState();
 }
 
 class _TodoDetailsScreenState extends State<TodoDetailsScreen> {
+  TodoDetailsViewModel get viewModel => widget.viewModel;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Listando detalhe do TODO')),
-      body: Center(child: Text('Todo ${widget.id}')),
+      body: ListenableBuilder(
+        listenable: viewModel.load,
+        builder: (context, child) {
+          if (viewModel.load.running) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (viewModel.load.error) {
+            return const Center(child: Text('Erro ao carregar TODO'));
+          }
+
+          return child!;
+        },
+        child: ListenableBuilder(
+          listenable: viewModel,
+          builder: (context, child) {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TodoName(title: viewModel.todo.title),
+            );
+          },
+        ),
+      ),
     );
   }
 }
