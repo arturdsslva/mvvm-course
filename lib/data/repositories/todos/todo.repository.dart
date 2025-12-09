88cd9e7085dadc692f1ab8376f8eb78aefc2f949
arturdsslva <arturdsslva@gmail.com>
2025-12-09 00:21:58 -0300

    refactor repo

--- lib/data/repositories/todos/todo.repository.dart
+++ lib/data/repositories/todos/todo.repository.dart
@@ -3,7 +3,17 @@ import '../../../domain/models/todo.model.dart';
 
 abstract class TodoRepository {
   Future<Result<List<Todo>>> get();
-  Future<Result<Todo>> create(String title);
-  Future<Result<void>> delete(Todo todo);
+  Future<Result<Todo>> create({
+    required String title,
+    required String desc,
+    required bool done,
+  });
+  Future<Result<void>> delete(String id);
   Future<Result<Todo>> getById(String id);
+  Future<Result<Todo>> update({
+    required String id,
+    required String title,
+    required String desc,
+    required bool done,
+  });
 }
